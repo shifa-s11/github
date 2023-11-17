d1c7a949ff22834811c7c0a568306c5262f20775
shifa-s11 <shifa221054074@akgec.ac.in>
2023-11-17 12:06:27 +0530

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import './App.css'
 import Nav from './components/Nav'
 import Users from './Routes/Users'
 import UsersData from './Routes/UsersData'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-// import { Routes,Route } from 'react-router-dom'
+import { Routes,Route } from 'react-router-dom'
 
 function App() {
 
 
   return (
     <>
+    
       <Nav/>
     <Routes>
 <Route path='/github/' Component={Users} />
